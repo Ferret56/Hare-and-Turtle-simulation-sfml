cd626566f168cbc8c6305b868e5bae7758ee8cf7
Ferret56 <Ferret56@yandex.ru>
2019-07-26 21:33:58 +0300

    remove whitespace

--- app/src/Hare.cpp
+++ app/src/Hare.cpp
@@ -1,6 +1,4 @@
 #include "Hare.h"
 
-
-
 Hare::Hare(sf::Vector2f position, const std::string path_to_the_file) : Animal(position, path_to_the_file){}
 Hare::~Hare(){}
--- app/src/Turtle.cpp
+++ app/src/Turtle.cpp
@@ -1,7 +1,6 @@
 #include "Turtle.h"
 
 Turtle::Turtle(sf::Vector2f position, const std::string path_to_the_file) : Animal(position, path_to_the_file){}
-
 Turtle::~Turtle() {}
 
 
