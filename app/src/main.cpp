3bc58ba5aa3ac70ffc1d2ff37c587df510fffe54
Ferret56 <Ferret56@yandex.ru>
2019-08-04 21:23:22 +0300

    #define size of the renderWindow

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,7 +1,10 @@
 #include "Simulation.h"
+#define MAX_RENDER_WINDOW_WIDTH  1280
+#define MAX_RENDER_WINDOW_HEIGHT 720
 
 int main(){
-	Simulation simulation(sf::Vector2u(1280,720),"Hare and Turtle simulation");
+	Simulation simulation(sf::Vector2u(MAX_RENDER_WINDOW_WIDTH,MAX_RENDER_WINDOW_HEIGHT),
+		                               "Hare and Turtle simulation");
 	simulation.run();
     return 0;
 }
