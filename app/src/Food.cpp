b45fad2a81eb1187881f8f3826fdeea0b41c81cb
Ferret56 <Ferret56@yandex.ru>
2019-08-04 22:51:09 +0300

    add description to the constructor update constructor create getter for local variable 'name'

--- app/src/Food.cpp
+++ app/src/Food.cpp
@@ -1,12 +1,18 @@
 #include "Food.h"
 
-Food::Food(sf::Vector2f position, const std::string path_to_the_file) {
+
+/*
+* @param name  the name of  the food ('apple' for ex.)
+*/
+Food::Food(sf::Vector2f position, const std::string path_to_the_file, const std::string name) {
 	this->foodTexture = new sf::Texture();
 	this->foodSprite = new sf::Sprite();
 	foodTexture->loadFromFile(path_to_the_file);
 	foodSprite->setTexture(*foodTexture);
 	this->foodPosition = position;
 	this->foodSprite->setPosition(position);
+	this->name = name;
+	
 }
 
 Food::~Food() {
@@ -16,6 +22,10 @@ Food::~Food() {
 
 void Food::draw(sf::RenderWindow* mainRenderWindow) {
 	mainRenderWindow->draw(*foodSprite);
+}
+
+std::string Food::getname() const {
+	return this->name;
 }
 
 
