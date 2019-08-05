f1116a9427ed65e3adc1eb40a6a04dfa2c9bb8dc
Ferret56 <Ferret56@yandex.ru>
2019-08-06 01:32:04 +0300

    create

---
+++ app/src/PackageOfFood.cpp
@@ -0,0 +1,49 @@
+#include "PackageOfFood.h"
+/*
+	* @param     foodObj     is a pointer to the Food class whose
+	*                                 object we want to generate
+	*
+	* @param     trackObj    is a pointer to a Track class object
+	*                             where we want to generate food
+	*
+	* @param     amount      is the amount of food we want to generate
+	*
+	* This constructor calls the 'generateFood' method
+*/
+PackageOfFood::PackageOfFood(Food* foodObj, Track* trackObj, unsigned int amount) {
+	generateFood(foodObj, trackObj, amount);
+}
+PackageOfFood::~PackageOfFood() {
+	for (auto food : packageOfFood)
+		delete food.second;
+}
+
+/*
+	* @param     foodObj     is a pointer to the Food class whose
+	*                                 object we want to generate
+	*
+	* @param     trackObj    is a pointer to a Track class object
+	*                             where we want to generate food
+	*
+	* @param     amount      is the amount of food we want to generate
+*/
+void PackageOfFood::generateFood(Food* foodObj, Track* trackObj, unsigned int amount) {
+	//Generate food in the range from 100......to FinishFlag position - 50px	
+	sf::Vector2f  current_food_position = {0,trackObj->getPosition().y + 20};
+
+	for (int i = 0; i < amount; ++i) {
+		current_food_position.x = rand() % ((int)trackObj
+			                               ->getFinishFlag()
+		                                   ->getSprite()
+			                               ->getPosition().x) - 50 + 100;
+
+		packageOfFood[i] = new Food(current_food_position, foodObj->getName(),
+			                             foodObj->getAbsolutePathToTheFile());
+	}		
+}
+
+void PackageOfFood::draw(sf::RenderWindow* window) {
+	for (auto food : packageOfFood)
+		window->draw(food.second->getSprite());
+}
+
---
+++ app/src/PackageOfFood.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include<String>
+#include "Food.h"
+#include "Track.h"
+/*
+* This class represents a package of several types of food
+* A package of food (several pieces) is on a certain animal's path
+*/
+class PackageOfFood{
+private:
+	//std::map of several types of food
+	//               WHERE:
+	//unsigned int  is the serial number of the food
+	//Food*         is the food itself
+	std::map<unsigned int, Food*>    packageOfFood;
+public:
+	/*
+	* @param     foodObj     is a pointer to the Food class whose
+	*                                 object we want to generate
+	*
+	* @param     trackObj    is a pointer to a Track class object
+	*                             where we want to generate food
+	*
+	* @param     amount      is the amount of food we want to generate
+	*/	
+	PackageOfFood(Food* foodObj, Track* trackObj, unsigned int amount);
+	~PackageOfFood();
+	/*
+	* @param     foodObj     is a pointer to the Food class whose
+	*                                 object we want to generate
+	*
+	* @param     trackObj    is a pointer to a Track class object
+	*                             where we want to generate food
+	*
+	* @param     amount      is the amount of food we want to generate
+	*/
+	void generateFood(Food* foodObj, Track* trackObj, unsigned int amount);
+	void draw(sf::RenderWindow* window);
+	
+
+};
+
