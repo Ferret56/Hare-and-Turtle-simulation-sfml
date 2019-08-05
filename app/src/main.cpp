66e16c01e29742d5fc4504dfc14b46f467acc4b6
Ferret56 <Ferret56@yandex.ru>
2019-08-06 01:28:11 +0300

    added time dependency

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -3,6 +3,7 @@
 #define MAX_RENDER_WINDOW_HEIGHT 720
 
 int main(){
+	     srand(time(NULL));
          Simulation simulation(sf::Vector2u(MAX_RENDER_WINDOW_WIDTH,MAX_RENDER_WINDOW_HEIGHT),
 		                               "Hare and Turtle simulation");
          simulation.run();
