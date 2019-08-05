bb1ffaa9a1d26126460899249b9f82b0f889797b
Ferret56 <Ferret56@yandex.ru>
2019-08-06 01:39:16 +0300

    delete whitespaces

--- app/src/PackageOfFood.cpp
+++ app/src/PackageOfFood.cpp
@@ -17,7 +17,6 @@ PackageOfFood::~PackageOfFood() {
 	for (auto food : packageOfFood)
 		delete food.second;
 }
-
 /*
 	* @param     foodObj     is a pointer to the Food class whose
 	*                                 object we want to generate
@@ -30,13 +29,11 @@ PackageOfFood::~PackageOfFood() {
 void PackageOfFood::generateFood(Food* foodObj, Track* trackObj, unsigned int amount) {
 	//Generate food in the range from 100......to FinishFlag position - 50px	
 	sf::Vector2f  current_food_position = {0,trackObj->getPosition().y + 20};
-
 	for (int i = 0; i < amount; ++i) {
 		current_food_position.x = rand() % ((int)trackObj
 		                                ->getFinishFlag()
 		                                ->getSprite()
 		                                ->getPosition().x) - 50 + 100;
-
 		packageOfFood[i] = new Food(current_food_position, foodObj->getName(),
 			                             foodObj->getAbsolutePathToTheFile());
 	}		
